96170a85a839b4c4a87e9639bd2bb3f800e35a5c
Chase Dorsey <git@chase-dorsey.com>
2017-11-26 12:03:43 -0600

    Let's try again

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import VueTouch from 'vue-touch'
 import App from './App.vue'
 import 'fullpage.js'
 
+Vue.use(VueTouch);
+
 const app = new Vue({
   el: '#app',
   render: h => h(App),
 });
-
-app.use(VueTouch);
